4c63930c25f72ec722a499025f9a75d0138b9fe3
Your Name <you@example.com>
2026-01-30 11:56:28 -0800

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar";
 
-function hello() {
+function App() {
   return (
     <>
-    <h1 className="text-blue-700">hello </h1>
+    <Navbar />
     </>
   )
 }
 
-export default hello;
+export default App;
